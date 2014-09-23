b538d4237edd4da555cad0f274a7e50175f262b9
Sämy Zehnder <saemy.zehnder@gorrion.ch>
2014-09-23 19:05:13 +0200

    Nicer login page layout.

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -13,21 +13,38 @@
             </div>
         @endif
 
-        <form method="post" action="{{ url('login') }}" class="form-horizontal">
-            <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+        <form method="post" action="{{ url('login') }}">
+            <fieldset>
+                <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
 
-            <p>
                 <input type="text" id="username" name="username"
                     placeholder="{{ Lang::get('secretstore.login_username') }}"
-                    value="{{ Input::old('username') }}">
-            </p>
-            <p>
+                    value="{{ Input::old('username') }}"
+                    class="text ui-widget-content ui-corner-all">
                 <input type="password" id="password" name="password"
-                    placeholder="{{ Lang::get('secretstore.login_password') }}">
-            </p>
-            <button type="submit" class="btn">
-                {{ Lang::get('secretstore.login_sign_in') }}
-             </button>
+                    placeholder="{{ Lang::get('secretstore.login_password') }}"
+                    class="text ui-widget-content ui-corner-all">
+
+                <button type="submit" class="btn">
+                    {{ Lang::get('secretstore.login_sign_in') }}
+                </button>
+            </fieldset>
         </form>
     </div>
+    <style>
+        #content {
+            margin: 0 auto;
+            max-width: 500px;
+        }
+
+        form input {
+            display: block;
+            margin: 5px auto;
+            width: 240px;
+        }
+
+        form button {
+            margin-top: 10px;
+        }
+    </style>
 @stop
